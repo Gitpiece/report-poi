2f55ec3fa00b66db25db9659cfb2d95d430f7ed8
whyDK37 <why.DK37@gmail.com>
2015-06-23 14:00:46 +0800

    修改提示信息。

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,5 +1,3 @@
-import org.testng.annotations.Test;
-
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Calendar;
@@ -16,7 +14,7 @@ import java.util.Scanner;
  */
 public class SolutionTest {
 
-    @Test
+
     public void test() {
         try {
             System.out.println(URLEncoder.encode("jdbc:db2://10.1.3.31:49500/aiispdb:currentSchema=aiis","utf-8"));
